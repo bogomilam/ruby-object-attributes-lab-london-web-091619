efd63e0db30a14ee5c1a9e127cfc44b9cd1736e3
bogomilmihaylov <bogomilmihaylov@live.com>
2019-09-17 19:28:43 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,9 @@
 class Dog 
-  def name(name)
-    @this_dogs_name
+  def name(dogs_name)
+    @name = name
   end
 def name=(dogs_name)
-    @this_dogs_name = dogs_name
+    @name = dogs_name
   end
  
 end   
